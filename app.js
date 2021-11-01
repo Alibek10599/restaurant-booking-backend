fe5e097246e27fb8bbc708ac85caa828164dab17
Alibek Khojabekov <alibek.khojabekov@nu.edu.kz>
2021-11-01 16:07:53 +0600

    some fixes for app to run

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 var cookieParser = require("cookie-parser");
  var logger = require("morgan");
- 
+ const express = require('express')
  var app = express();
- 
+ var cors = require('cors')
  app.use(logger("dev"));
  app.use(cors());
  module.exports = app;
