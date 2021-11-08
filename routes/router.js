a45855f4b9678a6c11f88f7b49abbfea30ef1a99
Alibek Khojabekov <alibek.khojabekov@nu.edu.kz>
2021-11-08 15:56:10 +0600

    orders renewed

--- routes/router.js
+++ routes/router.js
@@ -1,9 +1,11 @@
 const routes = require("express").Router();
 const user = require("./users");
+const orders = require ("./order")
 const restaraunts = require('./restaraunt')
 routes.get("/", async function (req, res) {
    res.send(`Reached home!`);
 });
 routes.use("/", user);
 routes.use('/', restaraunts)
+routes.use('/orders', orders)
 module.exports = routes;
