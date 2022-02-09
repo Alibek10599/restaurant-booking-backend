49cb6e6a8b50051a9bef458f952e823442496672
Alibek Khojabekov <alibek.khojabekov@nu.edu.kz>
2022-02-10 00:59:42 +0600

    upd devServer

--- devServer.js
+++ devServer.js
@@ -1,8 +1,11 @@
 const app = require("./app");
 const port = 4000 //process.env.PORT || 3000;
 
-const routes = require("./routes/router");
-app.use("/", routes);
+// const routes = require("./routes/router");
+// app.use("/", routes);
+app.get("/", async function (req, res) {
+   res.send(`Reached home!`);
+});
 app.listen( port ,function () {
    console.log("Server started. Go to http://localhost:" + port);
 });
