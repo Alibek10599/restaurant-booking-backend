53d67e3dd3994b1634ad2646134b06802dc8a749
altair.aimenov <altairaimenov@gmail.com>
2022-02-21 10:35:45 +0600

    Configured mysql connection to Amazon RDS

--- database-check.js
+++ database-check.js
@@ -4,9 +4,10 @@ console.log('Get connection ...');
 
 var conn = mysql.createConnection({
    database: 'bbo-db',
-   host: "bbo-db.cugn8zapjwmd.eu-west-1.rds.amazonaws.com:5000",
+   host: "bbo-db.cugn8zapjwmd.eu-west-1.rds.amazonaws.com",
    user: "admin",
-   password: "12345678"
+   password: "12345678",
+   port: 5000
 });
 
 conn.connect(function(err) {
